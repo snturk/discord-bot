21dda4d43152cd5b334376c0d84d4ce997787bb5
snturk <muratcansenturk2000@hotmail.com>
2020-10-04 22:57:07 +0300

    fixed bugs
    fixed command feedback message bugs and added play music feature

--- discord-bot.py
+++ discord-bot.py
@@ -1,8 +1,13 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
 import botToken
 
 client = commands.Bot(command_prefix='-')
+
+players = {}
 
 
 @client.event
@@ -25,33 +30,52 @@ async def on_message(message):
 async def gel(ctx):
     if ctx.message.author.voice:
         channel = ctx.message.author.voice.channel
-        print(channel, 'HAH')
         await channel.connect()
     else:
-        await ctx.message.channel.send('HAHA kanala katıl')
+        await ctx.message.channel.send('HAHA önce kanala katıl')
 
 
-@client.command(pass_context = True)
+@client.command(pass_context=True)
 async def git(ctx):
     if ctx.message.author.voice:
         server = ctx.message.guild.voice_client
         await server.disconnect()
+    else:
+        await ctx.message.channel.send('tamam da nerden?')
 
 
-@client.command()
+@client.command(pass_context=True)
+async def oynat(ctx, url):
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.is_playing()
+    else:
+        await ctx.send("dur bi")
+        return
+
+
+@client.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
-async def tekmele(self, ctx, member: discord.Member, *, reason=None):
+async def tekmele(ctx, member: discord.Member):
     await member.kick()
+    await ctx.message.channel.send(f'<@{member.id}> efendimiz tarafından tekmelendi')
 
 
-@client.command()
+@client.command(pass_context=True)
 @commands.has_permissions(ban_members=True)
-async def defol(self, ctx, member: discord.Member, *, reason=None):
+async def defol(ctx, member: discord.Member):
+    await ctx.send(f"{member} defoldu")
     await member.ban()
-    await ctx.channel.send(f"{user.mention} defoldu")
 
 
-@client.command()
+@client.command(pass_context=True)
 @commands.has_permissions(ban_members=True)
 async def defolma(ctx, *, member):
     banned_users = await ctx.guild.bans()
